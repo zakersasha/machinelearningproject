630e8a5cc2d34a26e442125aafc880d08b62b922
zakersasha <https://vk.com/getr3kt>
2020-04-06 21:41:34 +0500

    lab1

--- lab.py
+++ lab.py
@@ -1,130 +1,93 @@
-import pandas as pd
+import pandas
 
 import datetime
 
-import numpy as np
-
 from sklearn.model_selection import train_test_split
-
 from sklearn.linear_model import LogisticRegression
-
 from sklearn.metrics import precision_recall_fscore_support
 
-from sklearn.preprocessing import LabelEncoder
-
-# считываем файл с данными
-df = pd.read_csv('casc-resto.csv', sep=';', decimal=',', dtype='str')
-
-# приводим данные к "нормальному" виду
-df['SummBasic'] = [x.replace(',', '.') for x in df['Sum']]
-df['SummBasic'] = df['SummBasic'].astype(float)
-df['SummAfterPointsUsage'] = df['SummAfterPointsUsage'].astype(float)
-df['RKDate'] = pd.to_datetime(df['RKDate'], format='%Y-%m-%d')
-df['SummAfterPointsUsage'] = [x.replace(',', '.') for x in df['SummAfterPointsUsage']]
-df.drop_duplicates(keep=False, inplace=True)
-df.dropna()
+# Считываем файлы с данными
+df_1 = pandas.read_csv('casc-resto.csv', sep=';', decimal=',', dtype='str')
+df_2 = pandas.read_csv('CASC_Constant.csv', sep=',', decimal=',', dtype='str')
+df_3 = pandas.DataFrame(columns=['CustomerID', 'Frequency', 'Recency', 'Monetary Value', 'Mean Saved', 'Visit'])
 
 first_date = datetime.datetime(2017, 7, 1)
 last_date = datetime.datetime(2017, 12, 31)
 
-# сортировка по айди
-df.sort_values("CustomerID", inplace=True)
-
-# создаем датафреймы
-before = df[df['RKDate'] < first_date]
-after = df[df['RKDate'] >= first_date]
-
-orders = before.copy()
-orders.head()
-
-# Recency
-orders['dso'] = before['RKDate'].apply(lambda x: (first_date - x).days)
-orders['dsf'] = before['RKDate'].apply(lambda x: (first_date - x).days)
-aggr = {
-    'dso': lambda x: x.min(),
-    'dsf': lambda x: x.max(),
-    'RKDate': lambda x: len([d for d in x]),
-    'SummBasic': 'sum',
-    'SummAfterPointsUsage': 'sum',
-}
-
-# доп.задание
-df_1 = orders.groupby('CustomerID').agg(aggr).reset_index()
-df_1['Dop'] = df_1['SummAfterPointsUsage'] - df_1['SummBasic']
-
-# записать данные в файл
-df_1.to_csv('df_2.csv', index=False, header=True)
-
-# Recency, Frequency, Monetary Value
+# Период в 30 дней
 time = 30
-df_2 = pd.read_csv('df_2.csv', sep=',', decimal='.')
 
-df_2['SummBasic'].astype(float)
-df_2['SummAfterPointsUsage'].astype(float)
-df_2['visit'].astype(int)
-df_2['Monetary'] = df_2['SummBasic'] / df_2['visit']
-df_2['Frequency'] = df_2['visit'] / (
-        df_2['DaysSinceFirst'] / time)
-df['samedate'] = np.where((df['RKDate'] <= last_date) & (df['RKDate'] >= first_date), 1, 0)
-df['samedate'] = df['samedate'].astype('int')
-dfByDateMatch = df.groupby('CustomerID')['dateMatch'].agg(['sum'])
+# Приведение данных к "нормальному" виду
+df_2.rename(columns={'CustomerId': 'CustomerID'}, inplace=True)
 
-aggressive = {
-    'sum': lambda x: len([d for d in x if d > 0])
-}
-dfY = dfByDateMatch.groupby('CustomerID').agg(aggressive)
-dfY.rename(columns={'sum': 'Y', }, inplace=True)
-dfY['Y'] = dfY['Y'].astype(int)
+df_1['RKDate'] = pandas.to_datetime(df_1['RKDate'])
+
+df_1['SumBasic'] = df_1['SumBasic'].astype(float) # Приводим значения к вещественному типу
+df_1['SumBasic'] = [i.replace(',', '.') for i in df_1['SumBasic']] # Заамена запятых на точки
+
+df_1['SumAfterPointsUsage'] = df_1['SumAfterPointsUsage'].astype(float) # Приводим значения к вещественному типу
+df_1['SumAfterPointsUsage'] = [i.replace(',', '.') for i in df_1['SumAfterPointsUsage']] # Заамена запятых на точки
+
+# Количество раз, когда SumAfterPointsUsage меньше чем SumBasic
+use_freq = 0
+
+# Подсчет use_freq
+for item in df_1:
+    if df_1['SumAfterPointsUsage'] < df_1['SumBasic']:
+        use_freq += 1
+
+for a, b in df_1.groupby(['CustomerID']):
+    date = b[b['RKDate'] < first_date]
+
+    if not date.empty:
+        Frequency = date[0] / ((first_date - min(date['RKDate'])).days / time)
+
+        Recency = first_date - max(date['RKDate'])
+
+        MonetaryValue = date['SumBasic'].mean()
+
+        MeanSaved = (date['SumBasic'] - date['SumAfterPointsUsage']).mean()
+
+        df_1['Usage'] = date[0] / use_freq
+        # Заполняем датафрейм полями и данными
+        df_3 = df_3.append(
+            {'CustomerID': a,
+             'Frequency': Frequency,
+             'Recency': Recency,
+             'Monetary Value': MonetaryValue,
+             'Mean Saved': MeanSaved,
+             'Usage': df_1['Usage']}, ignore_index=True)
 
 
-df2 = df_2[['CustomerID', 'Recency', 'Frequency', 'Monetary', 'Dop']].copy()
-df2.set_index('CustomerID', inplace=True)
+# объединение 2 и 3 датафрейма в один df_res
+df_res = pandas.merge(df_3, df_2, on='CustomerID', how='left')
 
-dfCustomers = pd.read_csv('CASC_Constant.csv', sep=',')
-dfCustomers = dfCustomers.drop(columns=['ActivationDate', 'SubscribedEmail', 'SubscribedPush'])
+df_res['Age'] = df_res['Age'].astype(int)
+df_res['Sex'] = df_res['Sex'].astype(int)
+df_res['Usage'] = df_res['Usage'].astype(int)
 
-dfCustomers.sort_values("CustomerId", inplace=True)
-dfCustomers.rename(columns={"CustomerId": "CustomerID"}, inplace=True)
-dfCustomers.set_index('CustomerID', inplace=True)
+# добавляем условия для исключения выбросов
+if df_res[df_res.Monetary > 200]:
+    df_res = df_res.dropna()
+if df_res[df_res.Frequency < 20]:
+    df_res = df_res.dropna()
 
-full = pd.merge(df2, dfCustomers, how='left', on=['CustomerID'])
-full = pd.merge(full, dfY, how='left', on=['CustomerID'])
+y = df_res['Usage']
+merged = df_res.drop(columns=['Usage'])
 
-full['Age'] = full['Age'].astype(int)
-full['Y'] = full['Y'].astype(int)
-full = full[full.Monetary > 200]
-full = full[full.Frequency < 20]
-full = full.dropna()
+# Деление выборки на тестовую и тренировочную в соотношении 0.8 - 0.2
+xtrain, xtest, ytrain, ytest = train_test_split(df_res, y, test_size=0.2)
 
-label = LabelEncoder()
-dicts = {}
-label.fit(full.Sex.drop_duplicates())
-dicts['Sex'] = list(label.classes_)
-full.Sex = label.transform(full.Sex)
-full = full.reset_index()
-full = full.drop(columns=['CustomerID'])
+regres = LogisticRegression()
+regres.fit(xtrain, ytrain)
 
-y = full.Y
-full = full.drop(columns=['Y'])
-X = full
+res_train = regres.score(xtrain, ytrain)
+res_test = regres.score(xtest, ytest)
 
-# Разбив данные проверяем успешность предсказаний тестовых и тренировочных данных
-xtrain, xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2)
-
-model = LogisticRegression(solver='liblinear')
-model.fit(xtrain, ytrain)
-
-y_pred = model.predict(xtest)
-
-res_train = model.score(xtrain, ytrain)
-res_test = model.score(xtest, ytest)
+ypred = regres.predict(xtest)
 
 # !Вывод всех результатов!
-print(df_1[['CustomerID', 'Recency', 'DaysSinceFirst', 'NumOFVisits']].head())
-print(dfY)  # переменная y
-print(df2)  # Задание 2
-print(dfCustomers)  # По полу и возрасту
-print(full)  # frequency, recency ...
-print(res_train)  # успех на основе тренировочных данны
-print(res_test)  # успех на основе тестовых данны
-print(precision_recall_fscore_support(ytest, y_pred, average='macro'))  # Precision, recall based on Y
+print(res_train)
+print(res_test)
+print(ytest, ypred)
+print(precision_recall_fscore_support(ytest, ypred))
